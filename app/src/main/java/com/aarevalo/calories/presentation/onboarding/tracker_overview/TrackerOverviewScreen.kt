441f197bde6488ae136c3954cc661b25c618df76
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2025-01-15 22:34:55 -0800

    created tracker overview screen

---
+++ app/src/main/java/com/aarevalo/calories/presentation/onboarding/tracker_overview/TrackerOverviewScreen.kt
@@ -0,0 +1,46 @@
+package com.aarevalo.calories.presentation.onboarding.tracker_overview
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aarevalo.calories.ui.theme.LocalSpacing
+
+@Composable
+fun TrackerOverviewScreen() {
+
+    val spacing = LocalSpacing.current
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(spacing.spaceLarge)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Tracker Overview Screen",
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.titleSmall
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TrackerOverviewScreenPreview() {
+    TrackerOverviewScreen()
+}
